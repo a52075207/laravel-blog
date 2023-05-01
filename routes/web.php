e311603ae0fcb03d85e25ef46ca0205bcdd2822e
Rely <shark90323@gmail.com>
2023-05-01 11:36:51 +0800

    Company pract

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Role;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/admin/user/roles',[ 'middleware'=>'role' , function() {
+    return "Middleware role";
+}]);
+
+Route::get('/admin', [AdminController::class, 'index']);
